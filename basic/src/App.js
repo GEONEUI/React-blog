1a7e51e5e3304b053148d25d9d0a7d47c585afb2
GEONEUI <dbrjsdml@naver.com>
2022-12-09 23:22:04 +0900

    state 바인딩 까지완료

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+
+	const onSubmit = () => {
+		console.log(title, body);
+	}
+
+	return (
+		<div className="container">
+			<div className="mb-3">
+				<label className="form-label">Title</label>
+				<input
+					className="form-control"
+					value={title}
+					onChange={(event) => {
+						setTitle(event.target.value);
+					}}
+				/>
+			</div>
+			<div className="mb-3">
+				<label className="form-label">Body</label>
+				<textarea
+					className="form-control"
+					value={body}
+					onChange={(event) => {
+						setBody(event.target.value);
+					}}
+					rows="20"
+				/>
+			</div>
+			<button
+				className="btn btn-primary"
+				onClick={onSubmit}
+			>
+				Post
+			</button>
+		</div>
+	);
 }
 
 export default App;
