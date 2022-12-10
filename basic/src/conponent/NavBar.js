2495c83e451503dfb8c9d019204a8737a1614528
GEONEUI <dbrjsdml@naver.com>
2022-12-10 20:05:32 +0900

    24받아온 데이터 화면에 출력하기

--- basic/src/conponent/NavBar.js
+++ basic/src/conponent/NavBar.js
@@ -1,5 +1,6 @@
 import {
-	Link
+	Link,
+	NavLink
 }
 	from 'react-router-dom';
 
@@ -10,7 +11,12 @@ const NavBar = () => {
 				<Link className="navbar-brand" to="/">Home</Link>
 				<ul className="navbar-nav">
 					<li className="nav-item">
-						<Link className="nav-link active" aria-current="page" to="/blogs">Blogs</Link>
+						<NavLink
+							activeClassName='active'
+							className="nav-link"
+							aria-current="page"
+							to="/blogs">Blogs
+						</NavLink>
 					</li>
 				</ul>
 			</div>
--- basic/src/pages/Listpage.js
+++ basic/src/pages/Listpage.js
@@ -1,6 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Listpage = () => {
+
+	const [post, setPost] = useState([]);
+
+	const getPost = () => {
+
+		axios.get("http://localhost:3001/posts").then((res) => {
+			setPost(res.data);
+		})
+
+	}
+
+	useEffect(() => {
+		getPost();
+	}, []);
+
+
+
+
 	return (
-		<div>Listpage</div>
+		<div>
+			<h1>List contents</h1>
+			{post.map((item) => {
+				return (
+					<div>{item.title}</div>
+				);
+			})}</div>
 	);
 }
 
