9d6e2bd63ef453e91053c554c3167c8389b2e658
GEONEUI <dbrjsdml@naver.com>
2022-12-10 14:40:59 +0900

    라우터

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
 
 
 function App() {
@@ -18,35 +24,48 @@ function App() {
 	}
 
 	return (
-		<div className="container">
-			<div className="mb-3">
-				<label className="form-label">Title</label>
-				<input
-					className="form-control"
-					value={title}
-					onChange={(event) => {
-						setTitle(event.target.value);
-					}}
-				/>
+		<Router>
+			<div>
+				<Link to="/">Home</Link>
+				<Link to="/blogs">Blogs</Link>
 			</div>
-			<div className="mb-3">
-				<label className="form-label">Body</label>
-				<textarea
-					className="form-control"
-					value={body}
-					onChange={(event) => {
-						setBody(event.target.value);
-					}}
-					rows="20"
-				/>
-			</div>
-			<button
-				className="btn btn-primary"
-				onClick={onSubmit}
-			>
-				Post
-			</button>
-		</div>
+			<Switch>
+				<Route path="/">HomePage</Route>
+				<Route path="/blogs">
+					<div className="container">
+						<div className="mb-3">
+							<label className="form-label">Title</label>
+							<input
+								className="form-control"
+								value={title}
+								onChange={(event) => {
+									setTitle(event.target.value);
+								}}
+							/>
+						</div>
+						<div className="mb-3">
+							<label className="form-label">Body</label>
+							<textarea
+								className="form-control"
+								value={body}
+								onChange={(event) => {
+									setBody(event.target.value);
+								}}
+								rows="20"
+							/>
+						</div>
+						<button
+							className="btn btn-primary"
+							onClick={onSubmit}
+						>
+							Post
+						</button>
+					</div>
+				</Route>
+			</Switch>
+		</Router>
+
+
 	);
 }
 
